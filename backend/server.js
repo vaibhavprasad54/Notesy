385544f91e004bc679ac5b5c07b09a2e8c90ec4b
Vaibhav Prasad <vaibhavprasad54@gmail.com>
2022-11-27 19:39:03 +0530

    Initialized Frontend and Backend

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+// Importing Express
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv")
+
+// Creating object of this imporeted package
+const app = express();
+dotenv.config();
+
+//Creating Endpoint
+app.get("/", (req, res) => {
+    res.send("API is running...");
+})
+
+//Fetching Notes
+app.get("/api/notes", (req, res) => {
+    res.json(notes);
+})
+
+//Fetching Single note through id
+app.get("/api/notes/:id", (req, res) => {
+    const note = notes.find((n) => 
+        n._id === req.params.id
+    )
+    res.send(note);
+})
+
+// Creating web server
+const PORT = process.env.PORT || 7000;
+app.listen(5000, console.log(`Server started on port ${PORT}`));
