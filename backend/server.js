96cb3357180108597df8e6deaabdf8dfef7f1044
Vaibhav Prasad <vaibhavprasad54@gmail.com>
2023-03-19 19:39:56 +0530

    Final Build
    Deploying Final build containing Frontend and Backend on a single PORT.

--- backend/server.js
+++ backend/server.js
@@ -6,39 +6,44 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const noteRoutes = require("./routes/noteRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path");
 
 // Creating object of this imported package
-const app = express();
 dotenv.config();
+
 connectDB();
+
+const app = express();
+
+
 app.use(express.json());                // Wrote this line to resolve error coming in Postman 
-
-
-//Creating First Endpoint / API
-// app.get("/", (req, res) => {
-//     res.send("API is running...");
-// })
-
-//Fetching Notes
-// app.get("/api/notes", (req, res) => {
-//     res.json(notes);
-// })
-
-//Fetching Single note through id
-    // app.get("/api/notes/:id", (req, res) => {
-    //     const note = notes.find((n) => 
-    //         n._id === req.params.id
-    //     )
-    //     res.send(note);
-    // })
 
 
 app.use('/api/users', userRoutes );                       //Initializing users route
 app.use('/api/notes', noteRoutes );                       //Initializing notes route
 
+
+// -----------Deployment---------------
+__dirname = path.resolve();
+if(process.env.NODE_ENV === "production"){              // If environment is "Production"
+    app.use(express.static(path.join(__dirname, "/frontend/build")));                   //Connecting backend route with frontend "build" folder route.
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+    });
+
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running..");
+      });
+}
+
+
 app.use(notFound);
 app.use(errorHandler);
 
+
+
 // Creating web server
 const PORT = process.env.PORT || 7000;
-app.listen(5000, console.log(`Server started on port ${PORT}`));
+app.listen(PORT, console.log(`${process.env.NODE_ENV} mode on port ${PORT}..`));
